c31fa5d13f4198333860721880da9d56aef4308a
stantlin s <stantilin@gmail.com>
2026-03-29 02:37:39 +0530

    logging url

--- src/features/auth/AuthAPI.js
+++ src/features/auth/AuthAPI.js
@@ -1,4 +1,6 @@
 export const loginWithGoogle = () => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL; 
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  console.log("Backend URL:", backendUrl);
+
   window.location.href = `${backendUrl}/api/auth/google`;
 };
